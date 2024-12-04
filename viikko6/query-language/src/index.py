5c11ac3601fc54aa5a4d95e088b3ac0375e1cfd2
neakovalainen <neakovalaine@gmail.com>
2024-12-04 15:47:28 +0200

    Exercise 3 of week 6

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,21 +1,36 @@
 from statistics import Statistics
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan
+from matchers import And, HasAtLeast, PlaysIn, Not, All, HasFewerThan, Or
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        Not(HasAtLeast(2, "goals")),
-        PlaysIn("NYR")
-    )
+    # matcher = And(
+    #     Not(HasAtLeast(2, "goals")),
+    #     PlaysIn("NYR")
+    # )
     
     # matcher = And(
     #     HasFewerThan(2, "goals"),
     #     PlaysIn("NYR")
     # )
+
+    # matcher = Or(
+    #     HasAtLeast(45, "goals"),
+    #     HasAtLeast(70, "assists")
+    # )
+    matcher = And(
+        HasAtLeast(70, "points"),
+        Or(
+            PlaysIn("NYR"),
+            PlaysIn("FLA"),
+            PlaysIn("BOS")
+        )
+    )
+
+
 
 
     for player in stats.matches(matcher):
